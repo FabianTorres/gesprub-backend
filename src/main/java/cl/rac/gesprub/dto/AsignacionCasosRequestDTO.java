b7866a059bc5e47401d589d61dad3683a7cd3175
FabianTorresD <fabiantorresd@gmail.com>
2026-02-19 14:50:39 -0300

    Se modifica el DTO de vincular casos a ciclos

--- src/main/java/cl/rac/gesprub/dto/AsignacionCasosRequestDTO.java
+++ src/main/java/cl/rac/gesprub/dto/AsignacionCasosRequestDTO.java
@@ -3,12 +3,12 @@ package cl.rac.gesprub.dto;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class AsignacionCasosRequestDTO {
     
-    @NotEmpty(message = "La lista de IDs de casos no puede estar vacía.")
+	@NotNull(message = "La lista de IDs no puede ser nula.")
     private List<Long> idsCasos;
 }
